0a10f5ab1651d2aad6dd017e37501073cb29f3e0
Kevin Coronato <kcoronato@gmail.com>
2019-12-23 13:05:06 -0500

    Added About_Job_Scraper.txt
    Added an about txt file.

--- JobScraper.py
+++ JobScraper.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.indeed.com/jobs?q=Software+Engineer&l=Raleigh%2C+NC#"
-indeedPage = requests.get(url)
+monsterUrl = "https://www.monster.com/jobs/search/?q=Software-Engineer&where=Raleigh__2C-NC&intcid=skr_navigation_nhpso_searchMain"
+indeedUrl = "https://www.indeed.com/jobs?q=Software+Engineer&l=Raleigh%2C+NC#"
+monsterPage = requests.get(monsterUrl)
+monsterSoup = BeautifulSoup(monsterPage.content, "html.parser")
+indeedPage = requests.get(indeedUrl)
 indeedSoup = BeautifulSoup(indeedPage.content, "html.parser")
 
-print(indeedSoup)
+# Validate host responds with 200 ok.
+if (monsterPage.status_code != 200):
+    quit
+else:
+    print(monsterPage.status_code)
+
+print(monsterSoup.prettify())
+
