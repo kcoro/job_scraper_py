8dc79ab9bd5f5756aa8205990999a25a7095f00c
Kevin Coronato <kcoronato@gmail.com>
2019-12-24 14:24:26 -0500

    Basic Implementation for Monster
    Working basic implementation for Monster website.  Scrapes job title, company name, location, and link to job desc.

--- JobScraper.py
+++ JobScraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = "Insert Fake User agent Here"
+
 monsterUrl = "https://www.monster.com/jobs/search/?q=Software-Engineer&where=Raleigh__2C-NC&intcid=skr_navigation_nhpso_searchMain"
 indeedUrl = "https://www.indeed.com/jobs?q=Software+Engineer&l=Raleigh%2C+NC#"
 monsterPage = requests.get(monsterUrl)
@@ -14,5 +16,37 @@ if (monsterPage.status_code != 200):
 else:
     print(monsterPage.status_code)
 
-print(monsterSoup.prettify())
+# Validate host responds with 200 ok.
+if (indeedPage.status_code != 200):
+    quit
+else:
+    print(indeedPage.status_code)
+
+# Loop over each product container on webpage
+for job in monsterSoup.find_all("div", class_="flex-row"):
+    # Each job title
+    jobTitle = job.find("a")
+    # Make object a string and strip lead and trail white space
+    jobTitle = jobTitle.text.strip()
+    # Strip trailing whitespace from job title
+    # Gets the jobs company name
+    jobCompany = job.find("div", class_="company")
+    jobCompany = jobCompany.find("span", class_="name")
+    # Make object into string and strip lead and trail whitespace
+    jobCompany = jobCompany.text.strip()
+    # Gets jobs location
+    jobLocation = job.find("div", class_="location")
+    jobLocation = jobLocation.find("span", class_="name")
+    # Turn object into string and strip lead and trail whitespace
+    jobLocation = jobLocation.text.strip()
+    # Link to job info
+    jobLink = job.find('a')['href']
+    # Strip lead and trail whitespace
+    jobLink = jobLink.strip()
+    # Prints job title, location and link to job
+    print(jobTitle)
+    print(jobCompany)
+    print(jobLocation)
+    print(jobLink)
+    print("\n")
 
