0962d007d4a3ae8635bfcb1d8007a855f375ae7a
Kevin Coronato <kcoronato@gmail.com>
2019-12-25 15:39:19 -0500

    Added implementation for StackOverflow Jobs
    Implementation to scrape job title, company, location and job link on stackoverflow jobs.

--- JobScraper.py
+++ JobScraper.py
@@ -53,7 +53,7 @@ for job in monsterSoup.find_all("div", class_="flex-row"):
     # Strip lead and trail whitespace
     jobLink = jobLink.strip()
 
-    # Prints job title, location and link to job
+    # Prints job title, company, location and link to job
     print(jobTitle)
     print(jobCompany)
     print(jobLocation)
@@ -79,7 +79,25 @@ for job in stackOverflowSoup.find_all("div", class_="-job js-dismiss-overlay-con
     jobTitle = job.find("h2", class_="fs-body3 mb4 fc-black-800")
     # Make object a string and strip lead and trail white space
     jobTitle = jobTitle.text.strip()
-    # Each job title
+    # Gets the jobs company name
+    jobCompany = job.find("h3", class_="fc-black-700 fs-body1 mb4")
+    jobCompany = jobCompany.find("span")
+    # Make object into string and strip lead and trail whitespace
+    jobCompany = jobCompany.text.strip()
+    # Gets jobs location
+    jobLocation = job.find("h3", class_="fc-black-700 fs-body1 mb4")
+    jobLocation = jobLocation.find("span", class_="fc-black-500")
+    # Turn object into string and strip lead and trail whitespace
+    jobLocation = jobLocation.text.strip()
+    # Link to job info
+    jobLink = job.find("a")["href"]
+    # Strip lead and trail whitespace
+    jobLink = jobLink.strip()
+
+    # Prints job title, company, location and link to job
     print(jobTitle)
+    print(jobCompany)
+    print(jobLocation)
+    print("https://stackoverflow.com"+jobLink)
     print("\n")
 
