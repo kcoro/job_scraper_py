ee5aa4090a3e7e32964ffd599f9166d5de5aa538
Kevin Coronato <kcoronato@gmail.com>
2020-01-12 16:28:07 -0500

    Added user input for job title and location
    Added user input to set job title and location.
    
    User input is added to each job sites url query parameters.

--- JobScraper.py
+++ JobScraper.py
@@ -2,10 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = "Insert Fake User agent Here"
-
 # Job Site URLs, use requests to get html from each URL.
 # Pass requests output to BeautifulSoup for parsing
-monsterUrl = "https://www.monster.com/jobs/search/?q=Software-Engineer&where=NC&tm=14"
+monsterUrl = "https://www.monster.com/jobs/search/?q=Software+Engineer&where=North-Carolina"
 stackOverflowUrl = "https://stackoverflow.com/jobs?q=Software+Engineer&l=North+Carolina%2C+USA&d=20&u=Miles"
 indeedUrl = "https://www.indeed.com/jobs?q=software+engineer&l=Raleigh,+NC&explvl=entry_level"
 # Use requests library to make get request to each websites URL
@@ -16,6 +15,20 @@ indeedPage = requests.get(indeedUrl)
 monsterSoup = BeautifulSoup(monsterPage.content, "html.parser")
 stackOverflowSoup = BeautifulSoup(stackOverflowPage.content, "html.parser")
 indeedSoup = BeautifulSoup(indeedPage.content, "html.parser")
+
+# Will prompt user to enter desired title and location
+# Remove spaces in users input with char's needed for search query
+print("Please enter desired job title:")
+userJobTitle = input()
+userJobTitle = userJobTitle.replace(" ","+")
+print("Please enter desired location:")
+userLocation = input()
+userLocation = userLocation.replace(" ","+")
+
+# Update job sites base url with user input query parameters
+monsterUrl = "https://www.monster.com/jobs/search/?q="+userJobTitle+"&where="+userLocation
+stackOverflowUrl = "https://stackoverflow.com/jobs?q="+userJobTitle+"&l="+userLocation+"%2C+USA&d=20&u=Miles"
+indeedUrl = "https://www.indeed.com/jobs?q="+userJobTitle+"&l="+userLocation+"&explvl=entry_level"
 
 # Validate each jobsite host responds with 200 ok.
 def validateSite(sitePage):
@@ -29,7 +42,7 @@ validateSite(monsterPage)
 validateSite(stackOverflowPage)
 validateSite(indeedPage)
 
-# Loop over jobs on Monster web page
+# Loop over jobs on Monster's page
 for job in monsterSoup.find_all("div", class_="flex-row"):
     # Each job title
     jobTitle = job.find("a")
